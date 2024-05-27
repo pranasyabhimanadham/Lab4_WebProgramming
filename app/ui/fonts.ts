5ddf95a16b0a0c6ff7d18a2b7332e8dcbc5d0682
pranasyabhimanadham <135666933+pranasyabhimanadham@users.noreply.github.com>
2024-05-27 15:44:25 -0400

    Save current work before running codemod

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Specify the weights you want to use
+});
